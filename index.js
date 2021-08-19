07b8598644faa8fd50b7a83cf3e8c89f5185dd96
mattjmason <masonmj10@gmail.com>
2021-08-19 01:11:16 -0400

    commit tree 7a59f1187fe45ce88d3697d19b41ac4e9e0c7d84

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(names) {
+    let thankYous = []
+    for ( let i = 0; i < names.length; i++) {
+        thankYous.push( `Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+
+return thankYous;
+
+}
+
+
+function countDown(firstNumber) {
+    while (firstNumber > 0) {
+        console.log(firstNumber);
+        firstNumber -= 1;
+    }
+    console.log(firstNumber);
+}
